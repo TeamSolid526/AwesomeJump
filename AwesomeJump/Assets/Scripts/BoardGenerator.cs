e1fcf1b872453ab6f42684679a17796d16bb02ef
Jiahao Li <jiahaoli@JiahaodeMBP.lan>
2021-09-16 20:02:45 -0700

    impl basic jumping user controller & board generator

---
+++ AwesomeJump/Assets/Scripts/BoardGenerator.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGenerator : MonoBehaviour
+{
+    public GameObject boardPrefab;
+    public Transform cameraPos;
+
+    public int numberOfPlatforms = 5;
+    public float spawnRange = 10f;
+    public float rangeX = 3f;
+    public float minY = 0.2f;
+    public float maxY = 1.5f;
+
+    private float generatedY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+
+        for (int i=0; i<numberOfPlatforms; i++) {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-rangeX, rangeX);
+            Instantiate(boardPrefab, spawnPosition, Quaternion.identity);
+        }
+
+        generatedY = spawnPosition.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(generatedY < cameraPos.position.y+spawnRange) {
+            Vector3 spawnPosition = new Vector3();
+            spawnPosition.y += generatedY + Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-rangeX, rangeX);
+            Instantiate(boardPrefab, spawnPosition, Quaternion.identity);
+            generatedY = spawnPosition.y;
+        }
+    }
+}
