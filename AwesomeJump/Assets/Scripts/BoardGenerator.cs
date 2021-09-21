83668a4fa24fc8011294ad2cacbdf1e395ec9e6a
cm56uw <cm56uw@gmail.com>
2021-09-20 22:40:40 -0700

    hook v1
    hook v1

--- AwesomeJump/Assets/Scripts/BoardGenerator.cs
+++ AwesomeJump/Assets/Scripts/BoardGenerator.cs
@@ -7,18 +7,18 @@ public class BoardGenerator : MonoBehaviour
     public GameObject boardPrefab;
     public Transform cameraPos;
 
-    public int numberOfPlatforms = 5;
+    public int numberOfPlatforms = 10;
     public float spawnRange = 10f;
-    public float rangeX = 3f;
+    public float rangeX = 1f;
     public float minY = 0.2f;
-    public float maxY = 1.5f;
+    public float maxY = 1f;
 
     private float generatedY;
     
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 spawnPosition = new Vector3();
+        Vector3 spawnPosition = Vector3.zero;
 
         for (int i=0; i<numberOfPlatforms; i++) {
             spawnPosition.y += Random.Range(minY, maxY);
--- AwesomeJump/Assets/Scripts/Player.cs
+++ AwesomeJump/Assets/Scripts/Player.cs
@@ -7,23 +7,63 @@ public class Player : MonoBehaviour
 {
     public float speed = 2f;
     public float movement = 0f;
+    public float ropeLength = 10f;
     Rigidbody2D rb;
+    Vector3 worldMousePosition;
+    Vector2 direction;
+    LineRenderer lr;
+    GameObject targetBoard;
+    bool hooked = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         movement = Input.GetAxis("Horizontal") * speed;
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (!hooked) {
+            direction = worldMousePosition - transform.position;
+            direction.Normalize();
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, transform.position + (Vector3)direction * ropeLength);
+        }
+
+        RaycastHit2D hitData = Physics2D.Raycast(new Vector2(worldMousePosition.x, worldMousePosition.y), Vector2.zero, 0);
+
+        if (hitData && Input.GetMouseButtonDown(0)) {
+            targetBoard = hitData.transform.gameObject;
+            float distance = ((Vector2)targetBoard.transform.position - (Vector2)transform.position).magnitude;
+            hooked = distance < ropeLength;
+            
+            
+        }
     }
 
     void FixedUpdate() {
-        Vector2 velocity = rb.velocity;
-        velocity.x = movement;
-        rb.velocity = velocity;
+        // Vector2 velocity = rb.velocity;
+        // velocity.x = movement;
+        // rb.velocity = velocity;
+        if (hooked) {
+            rb.velocity = direction * speed;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        direction = Vector2.zero;
+        hooked = false;
+        Vector3 p = other.transform.position;
+        p.y += 0.5f;
+        transform.position = p;
+        Vector2 v = rb.velocity;
+        v.x = 0f;
+        rb.velocity = v;
+        
     }
 }
