2fa01969a3150c7965f92565d4317314a6e9c9e9
Carl Bai <carlbai@CarldeMacBook-Pro.local>
2021-09-24 00:25:24 -0700

    Plus basic scoring

--- AwesomeJump/Assets/Scripts/Player.cs
+++ AwesomeJump/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     public float speed = 2f;
     public float movement = 0f;
+    public int health = 100;
     Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -25,5 +26,24 @@ public class Player : MonoBehaviour
         Vector2 velocity = rb.velocity;
         velocity.x = movement;
         rb.velocity = velocity;
+    }
+
+    // void OnTriggerEnter2D(Collider2D other) {
+    //     Debug.Log(other.gameObject.name);
+        
+    // }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        // Debug.Log(other.collider.gameObject.name);
+        // Rigidbody2D rb = other.collider.GetComponent<Rigidbody2D>();
+        // Debug.Log(rb.velocity.y);
+        
+        if (other.collider.gameObject.name == "Board(Clone)" && other.relativeVelocity.y > 0)
+        {
+            health ++;
+            Debug.Log(health);
+        }
+            
+        
     }
 }
