b9033af846db0f7da6b2944b05e4a6abbd94bc4a
Carl Bai <carlbai@usc-guestwireless-upc-newsc15280.usc.edu>
2021-09-27 17:17:15 -0700

    basic health

--- AwesomeJump/Assets/Scripts/Platform.cs
+++ AwesomeJump/Assets/Scripts/Platform.cs
@@ -59,12 +59,23 @@ public class Platform : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision) {
 
-        if (collision.relativeVelocity.y < 0){
+            if (collision.relativeVelocity.y < 0){
             Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
             if (rb != null) {
                 Vector2 velocity = rb.velocity;
                 velocity.y = JUMPFORCE;
                 rb.velocity = velocity;
+                
+            // // Debug.Log(health);
+            if(property.type=="buff"){
+            
+            // Debug.Log(property.value);
+            collision.collider.gameObject.GetComponent<Player>().health += property.value;
+            }
+            else{
+                collision.collider.gameObject.GetComponent<Player>().health -= property.value;
+            }
+            Debug.Log(collision.collider.gameObject.GetComponent<Player>().health);
             }
             // Reverse board property if colliding with player
             ReverseProperty();
