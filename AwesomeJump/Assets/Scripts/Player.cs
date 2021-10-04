27046bed03bb1b381bf089c9b7e481f15ad7ad43
USER <USER@DebbydeMacBook-Air.local>
2021-10-03 20:58:03 -0700

    modify Player.cs

--- AwesomeJump/Assets/Scripts/Player.cs
+++ AwesomeJump/Assets/Scripts/Player.cs
@@ -58,11 +58,6 @@ public class Player : MonoBehaviour
                 
                 
             }
-        }else{
-           
-             
-           //  JUMPFORCE = 10f;
-             
         }
     }
 
