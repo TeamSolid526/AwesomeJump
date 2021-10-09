7519faffda849c0965cc55f595e7aba53419e806
RainerYuan <36510771+RainerYuan@users.noreply.github.com>
2021-10-09 15:22:53 -0700

    GameOverScence (#25)
    created the GameOverScence

--- AwesomeJump/Assets/GameOver.cs
+++ AwesomeJump/Assets/GameOver.cs
@@ -11,12 +11,14 @@ public class GameOver : MonoBehaviour
     public Text scoreText;
     public Transform cameraPos;
     public Transform character;
+    public GameOverScene GameOverScene;
     // Start is called before the first frame update
     // Update is called once per frame
     void Update()
     {
         if(character.position.y < cameraPos.position.y - 6f || character.gameObject.GetComponent<Player>().health <= 0){
-            scoreText.text = "Game Over.";
+            //scoreText.text = "Game Over.";
+            GameOverScene.Setup();
         }
     }
 }
