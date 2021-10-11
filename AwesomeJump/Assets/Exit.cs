4a79a1cb84682dd6c43cb6b85e76cef56f56e3ed
Jiahao Li <jli64684@usc.edu>
2021-10-11 16:57:27 -0700

    add Main Menu Scene and scene switch (#30)
    Co-authored-by: Jiahao Li <jiahaoli@JiahaodeMacBook-Pro.local>

---
+++ AwesomeJump/Assets/Exit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
