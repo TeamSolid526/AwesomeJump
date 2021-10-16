4b6ae81a428d8177820e3d399df3c9bc4dd70518
JackyLiu47 <jackyliu9747@gmail.com>
2021-10-15 19:19:25 -0700

    new file:   AwesomeJump/Assets/Scripts/StartScene.cs 	new file:   AwesomeJump/Assets/Scripts/StartScene.cs.meta

---
+++ AwesomeJump/Assets/Scripts/StartScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize()
+    {
+        string startSceneName = "MainMenuScene";
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name.Equals(startSceneName))
+        {
+            return;
+        }
+        SceneManager.LoadScene(startSceneName);
+    }
+
+    
+}
