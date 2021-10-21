032019c5bd0adf34457c3f68dfc383734df120f3
USER <USER@DebbydeMacBook-Air.local>
2021-10-21 15:40:36 -0700

    add jupyter notebook

---
+++ downloadData.sh
@@ -0,0 +1,6 @@
+ PROJECT_ID=f196703e-9bf1-4465-824c-bb7767c129b2
+ API_KEY=34882b93bcb1a47dfdeff2560d3bd451
+ EXPORT_ID=caedec0d-048a-47e1-8ce4-ab59c1a5bccc
+#  REQUEST_JSON='{\"startDate\":\"2021-10-13\",\"endDate\":\"2021-10-16\",\"format\":\"json\",\"dataset\":\"custom\"}'
+# curl --user f196703e-9bf1-4465-824c-bb7767c129b2:34882b93bcb1a47dfdeff2560d3bd451 --request POST --header "Content-Type: application/json" --data '{"startDate":"2021-10-16","endDate":"2021-10-20","format":"json","dataset":"custom"}' https://analytics.cloud.unity3d.com/api/v2/projects/f196703e-9bf1-4465-824c-bb7767c129b2/rawdataexports
+curl --user f196703e-9bf1-4465-824c-bb7767c129b2:34882b93bcb1a47dfdeff2560d3bd451 https://analytics.cloud.unity3d.com/api/v2/projects/f196703e-9bf1-4465-824c-bb7767c129b2/rawdataexports/42d6898f-bfdf-4f94-ab63-b9d22c45adb1
