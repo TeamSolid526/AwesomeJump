260c428f584b9c3c5f76f544e6b5348647c5852f
USER <USER@LAPTOP-BG83IQN7.usc.edu>
2021-11-15 15:24:45 -0800

    resolve conflict

--- AwesomeJump/Assets/Scripts/Score.cs
+++ AwesomeJump/Assets/Scripts/Score.cs
@@ -32,28 +32,9 @@ public class Score : MonoBehaviour
         if(character.position.y > max){
             max = (int)character.position.y;
         }
-        //curTimeRemain -= Time.deltaTime;
-		curTimeRemain = GameObject.Find("LaserGenerator").GetComponent<LaserGenerator>().timer;
-        //if(curTimeRemain < 0){
-        //    curTimeRemain = timeRemain;
-        //}
-        //curTimeRemain -= Time.deltaTime;
-        bool flag = false;
-        character2 = GameObject.Find("Character");
-		score = character2.GetComponent<Player>();
-		flag = score.jump;
-        //flag = character2.GetComponent(typeof(bool)).hooked;
-        if(!flag){
-            hookNum = 1;
-        }else{
-            hookNum = 0;
-        }
-        // Debug.Log("max"+max);
-        //scoreOnly.text = "Score: " + max.ToString("0") + "\n";
-        scoreText.text = "Score: " + max.ToString("0") + "\n" +
-         "Hooks: " + hookNum.ToString() + "\n" +
-         "Laser in " + curTimeRemain.ToString("0") + "s";
-        maxScore = max;
-        //Debug.Log(character.position.y);gi
+
+        scoreText.text = "Score: " + max.ToString("0");
+
+        //Debug.Log(character.position.y);
     }
 }
